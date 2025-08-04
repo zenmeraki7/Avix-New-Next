4340331933b36673c7808f40b16a2c10be5ca285
zenmeraki7 <zenmerakihelp@gmail.com>
2025-08-04 16:14:12 +0530

    pages added

---
+++ avix-new/app/About/page.tsx
@@ -0,0 +1,28 @@
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+import HeroSection from "../components/Home/HeroSection";
+import FoundationSection from "../components/About/FoundationSection";
+import ValuesSection from "../components/About/ValuesSection";
+import JourneySection from "../components/About/JourneySection";
+import WhyChooseUsSection from "../components/Home/WhyChooseUsSection";
+import CTASection from "../components/Home/CTASection";
+
+
+
+const About = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+        <HeroSection />
+        <FoundationSection />
+        <ValuesSection />
+        <JourneySection />
+        <WhyChooseUsSection />
+        <CTASection/>
+    
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
---
+++ avix-new/app/Insurance/page.tsx
@@ -0,0 +1,25 @@
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+import HeroSection from "../components/Insurance/HeroSection";
+import StatsSection from "../components/Insurance/StatsSection";
+import FeatureSection from "../components/Insurance/FeatureSection";
+import ServiceSection from "../components/Insurance/ServiceSection";
+import WhyChooseSection from "../components/Insurance/WhyChooseSection";
+import CTASection from "../components/Insurance/CTASection";
+
+const Insurance = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Header />
+      <HeroSection />
+      <StatsSection />
+      <FeatureSection />
+      <ServiceSection />
+      <WhyChooseSection />
+      <CTASection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Insurance;
