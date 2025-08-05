da0119c1f9a8c4ad597cdfd098c6d002da491419
zenmeraki7 <zenmerakihelp@gmail.com>
2025-08-05 11:27:36 +0530

    main

--- avix-new/app/layout.tsx
+++ avix-new/app/layout.tsx
@@ -1,5 +1,4 @@
 // app/layout.tsx
-import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Header from './components/layout/Header';
@@ -18,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" suppressHydrationWarning={true}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <Header />
         <main>{children}</main>
         <Footer />
